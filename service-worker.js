d502fef4be36d3963169f68036686bbd1144bf32
PEDRO HENRIQUE ALVES DA FONSECA <pedroceduc132@gmail.com>
2025-09-23 10:08:24 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "horarios-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Instala e guarda no cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativa e remove caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Busca do cache primeiro (offline)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
