84f0f9ddf12dd0011fb12076b34a23587744b06d
PEDRO HENRIQUE ALVES DA FONSECA <pedroceduc132@gmail.com>
2025-09-28 14:53:31 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,39 @@
+const CACHE_NAME = "appvendas-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icone-192.png",
+  "./icons/icone-512.png"
+];
+
+// Instalando o Service Worker
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("app-saudacao-cache").then(cache => {
-            return cache.addAll([
-                "index.html",
-                "manifest.json",
-                "icone-192.png",
-                "icone-512.png",
-                "imagemManha.jpeg",
-                "imagemTarde.jpeg",
-                "imagemNoite.jpeg",
-                "imagemDescansar.jpeg"
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// Ativando e limpando caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(cache => cache !== CACHE_NAME)
+          .map(cache => caches.delete(cache))
+      )
+    )
+  );
+});
+
+// Interceptando requisições para funcionar offline
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
