eb628c2e46457bddcb0eae56b90be16267dadac8
PEDRO HENRIQUE ALVES DA FONSECA <pedroceduc132@gmail.com>
2025-10-06 08:12:34 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,36 +3,21 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icone-192.png",
-  "./icons/icone-512.png"
+  "https://cdn-icons-png.flaticon.com/512/891/891419.png",
+  "https://cdn-icons-png.flaticon.com/512/3144/3144456.png"
 ];
 
-// Instalando o Service Worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Ativando e limpando caches antigos
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(cache => cache !== CACHE_NAME)
-          .map(cache => caches.delete(cache))
-      )
-    )
-  );
-});
-
-// Interceptando requisições para funcionar offline
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
